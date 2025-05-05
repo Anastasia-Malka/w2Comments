8b8198cf1d34707ad7eff1fa1d285dcde241259f
Anastasia-Malka <a7kraft@gmail.com>
2025-05-05 00:01:48 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,3 +2,19 @@
 
 /* Автор:
 Anastasia Malka */
+
+let myName = "Anastasia"
+let mySurname = 'Malka'
+let myFavoriteDrink = 'coffee'
+let myFavoriteAnimal = 'cat'
+let myFavoriteProgrammingLanguage = 'JavaScript'
+
+let home = "LA"
+home = "santa clarita"
+
+console.log(myName)
+console.log(mySurname)
+console.log(myFavoriteDrink)
+console.log(myFavoriteAnimal)
+console.log(myFavoriteProgrammingLanguage)
+console.log(home)
